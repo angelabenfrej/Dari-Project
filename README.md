100fa92e9976ef8c0cc8928185b95c75f880cbc6
angelabenfrej <141182585+angelabenfrej@users.noreply.github.com>
2024-02-26 21:54:32 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# Dari-Project
+
+
+
+Dari is a modern real estate web application built with the MERN (MongoDB, Express.js, React, Node.js) stack. It provides a platform for users to search, list, book,  and explore various properties, whether for sale or rent. The application offers a user-friendly interface, advanced search options, and a seamless user experience.
+
+
+
+## Features
+
+- *Property Listings*: View a wide range of property listings with detailed information, including descriptions, prices, addresses, and more.
+
+- *Advanced Search*: Customize your property search by type (rent/sale), amenities, sorting options, and more.
+
+- *User Profiles*: Registered users can create and manage their profiles with profile pictures and listings also.
+
+- *Authentication*: Secure user registration and login functionality with Google OAuth.
+
+- *Book your Visit*: you can also select the date at which you want to visit the site.
+
+- *Maps and Location Information*: Integrating maps and providing location information is crucial for real estate and hospitality websites. Interactive maps can show the location of properties, nearby amenities (e.g., schools, restaurants, hospitals), transportation options, and other points of interest. This helps users understand the neighborhood and make informed decisions.
+
+- *Mark as Favourites*: you can also mark as favourite the property which you like.
+
+- *Property Details and Descriptions*: Each property listing should have comprehensive details and descriptions, including property features, room dimensions, amenities, nearby attractions, and any unique selling points. High-quality images and virtual tours can provide a more immersive experience for users.
+
+
+
+## Installation
+
+1. Clone the repository:
+
+   bash
+   git clone
+   
+
+2. Change into the project directory:
+   bash
+   cd Dari-Project
+   
+3. Install server dependencies:
+
+   bash
+   npm install
+   
+
+4. Install client dependencies:
+
+   bash
+   cd client
+   npm install
+   
+
+5. Create a .env file in the root directory with your configuration settings.
+
+   bash
+   MONGO = "MONGODB URI"
+   JWT_SECRET = "JWTSECRET"
+   
+
+6. Start the development server:
+
+   bash
+   npm run dev
+   
+
+## Usage
+
+- Explore property listings, customize your search, and create a user profile to list properties or access additional features."# Dari-Project" 
---
+++ client/src/components/Value/Value.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+  AccordionItemState,
+} from "react-accessible-accordion";
+import "react-accessible-accordion/dist/fancy-example.css";
+import {
+  MdOutlineArrowDropDown,
+  MdOutlineArrowDropDownCircle,
+} from "react-icons/md";
+import data from "../../utils/accordion.jsx";
+import "./Value.css";
+// Demo styles, see 'Styles' section below for some notes on use.
+
+const Value = () => {
+  return (
+    <section id="value" className="v-wrapper">
+      <div className="paddings innerWidth flexCenter v-container">
+        {/* left side */}
+        <div className="v-left">
+          <div className="image-container">
+            <img src="./8c4a2c3b521f4f19b93865a30283b5dc.jpg" alt="" />
+          </div>
+        </div>
+
+        {/* right */}
+        <div className="flexColStart v-right">
+          <span className="orangeText">Our Value</span>
+
+          <span className="primaryText">Value We Give to You</span>
+
+          <span className="secondaryText">
+            We are always ready to help by providing the best services for you.
+            <br />
+            We believe a good blace to live can make your life better
+          </span>
+
+          <Accordion
+            className="accordion"
+            allowMultipleExpanded={false}
+            preExpanded={[0]}
+          >
+            {data.map((item, i) => {
+              const [className, setClassName] = useState(null);
+              return (
+                <AccordionItem className={`accordionItem ${className}`} uuid={i} key={i}>
+                  <AccordionItemHeading>
+                    <AccordionItemButton className="flexCenter accordionButton ">
+                        {/* just for getting state of item */}
+                      <AccordionItemState>
+                        {({ expanded }) =>
+                          expanded
+                            ? setClassName("expanded")
+                            : setClassName("collapsed")
+                        }
+                      </AccordionItemState>
+                      <div className="flexCenter icon">{item.icon}</div>
+                      <span
+                        className="primaryText"
+                      >
+                        {item.heading}
+                      </span>
+                      <div className="flexCenter icon">
+                        <MdOutlineArrowDropDown size={20} />
+                      </div>
+                    </AccordionItemButton>
+                  </AccordionItemHeading>
+                  <AccordionItemPanel>
+                    <p className="secondaryText">{item.detail}</p>
+                  </AccordionItemPanel>
+                </AccordionItem>
+              );
+            })}
+          </Accordion>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Value;
